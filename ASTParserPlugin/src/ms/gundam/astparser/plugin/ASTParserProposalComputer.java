797a9b3040f52ff4826190e32938e40211402dd2
Tetsuo Yamamoto <tetsuo@cs.ce.nihon-u.ac.jp>
2012-05-03 03:46:04 +0900

    fix

--- ASTParserPlugin/src/ms/gundam/astparser/plugin/ASTParserProposalComputer.java
+++ ASTParserPlugin/src/ms/gundam/astparser/plugin/ASTParserProposalComputer.java
@@ -2,11 +2,15 @@ package ms.gundam.astparser.plugin;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import ms.gundam.astparser.DB;
 import ms.gundam.astparser.Value;
+import ms.gundam.astparser.ValuewithRanking;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
@@ -17,13 +21,17 @@ import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.QualifiedType;
 import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.ui.text.java.ContentAssistInvocationContext;
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposalComputer;
 import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
@@ -32,18 +40,65 @@ import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.contentassist.IContextInformation;
 
 public class ASTParserProposalComputer implements IJavaCompletionProposalComputer {
-	private int targetmethodoffset;
 	private String myClassname = null;
 	final private String directory = "/DB/mDB";
 	private DB db;
 
 	class MethodVisitor extends ASTVisitor {
-    	private List<Value> statementList = new ArrayList<Value>();
+    	private List<Value> beforestatementList = new ArrayList<Value>();
+    	private List<Value> afterstatementList = new ArrayList<Value>();	
+    	private int offset;
     
-		public List<Value> getStatementList() {
-			return statementList;
+		public MethodVisitor(int offset) {
+			super();
+			this.offset = offset;
 		}
 
+		public List<Value> getBeforeStatementList() {
+			return beforestatementList;
+		}
+
+		public List<Value> getAfterStatementList() {
+			return afterstatementList;
+		}
+
+    	public boolean visit(ClassInstanceCreation node) {
+			String classname = "";
+    		Type classtype = node.getType();
+    		if (classtype != null) {
+    			ITypeBinding type = classtype.resolveBinding();
+    			if (type != null) {
+					if (type.isArray()) {
+						classname = DB.ARRAYNAME;
+					} else {
+						classname = type.getQualifiedName();
+					}
+    			} else {
+    				if (classtype.isSimpleType()) {
+						classname = ((SimpleType)classtype).getName().getFullyQualifiedName();
+    				} else if (classtype.isQualifiedType()) {
+						classname = ((QualifiedType)classtype).getName().getFullyQualifiedName();
+    				} else
+    					;
+    			}
+    		} else {
+    			System.err.println("cannot get type of new statement.");
+    		}
+    		if (offset < node.getStartPosition()) {
+    			if (afterstatementList != null) {
+    				afterstatementList.add(new Value(classname, "<init>"));
+    			}
+    		} else {
+    			if (beforestatementList != null) {
+    				beforestatementList.add(new Value(classname, "<init>"));
+    			}
+    		}
+			return super.visit(node);
+    	}
+
+    	/**
+		 * メソッド呼び出し文を出現順にリストに追加していく
+		 */
 		public boolean visit(MethodInvocation node) {
 			String classname = "";
     		Expression exp = node.getExpression();
@@ -70,8 +125,14 @@ public class ASTParserProposalComputer implements IJavaCompletionProposalCompute
     		} else {
     			classname = myClassname;
     		}
-    		if (getStatementList() != null) {
-    			getStatementList().add(new Value(classname, node.getName().toString()));
+    		if (offset < node.getStartPosition()) {
+    			if (afterstatementList != null) {
+    				afterstatementList.add(new Value(classname, node.getName().toString()));
+    			}
+    		} else {
+    			if (beforestatementList != null) {
+    				beforestatementList.add(new Value(classname, node.getName().toString()));
+    			}
     		}
 			return super.visit(node);
 		}
@@ -93,16 +154,44 @@ public class ASTParserProposalComputer implements IJavaCompletionProposalCompute
 	public void sessionEnded() {
 	}
 
-	private List<Value> fetchSource(List<Value> list) {
-		final List<Value> ret = new ArrayList<Value>();
-		Value key = list.get(list.size()-1);
-System.out.println(key.getClassname() + "#" + key.getMethodname());
-		List<Value> result = db.get(key.getClassname(), key.getMethodname(), true);
-		for (Value v : result) {
-//System.out.println("\t"+ v.getClassname() + "#" + v.getMethodname());
-			ret.add(v);
+	private void makeRanking(Value key, Map<String, ValuewithRanking> proposalmap, boolean which) {
+		int ranking = 0;
+		int count = -1;
+
+		List<Value> result = db.get(key.getClassname(), key.getMethodname(), which);
+		Collections.sort(result);
+		long sum = 0;
+		for (Value v: result) {
+			sum += v.getCount();  
 		}
-		return ret;
+System.out.println(key.getClassname()+key.getMethodname());			
+		/*
+		 * 出現数順にランキングを1位からつける．すでにマップにある場合はランキングを統合する
+		 */
+		for (Value v : result) {
+			if (count != v.getCount()) {
+				count = v.getCount();
+				ranking++;
+			}
+			int percentage = (int) (v.getCount() * 100 / sum);
+			if (percentage == 0)
+				continue;
+			ValuewithRanking newvalue = new ValuewithRanking(v);
+			newvalue.setPercentage(percentage);
+			newvalue.setRanking(ranking);
+
+			String keyString = newvalue.getClassname()+"#"+newvalue.getMethodname(); 
+System.out.println(newvalue.getRanking()+":"+ keyString);			
+
+			if (proposalmap.containsKey(keyString)) {
+				ValuewithRanking value = proposalmap.get(keyString);
+				value.setRanking(value.getRanking() + ranking);
+				value.setPercentage(value.getPercentage() + percentage);
+				proposalmap.put(keyString, value);
+			} else {
+				proposalmap.put(keyString, newvalue);
+			}
+		}
 	}
 	
 	@Override
@@ -120,7 +209,7 @@ System.out.println(key.getClassname() + "#" + key.getMethodname());
 			if (method == null) {
 				return list;
 			}
-			targetmethodoffset = method.getSourceRange().getOffset();
+			method.getSourceRange().getOffset();
 		} catch (UnsupportedOperationException e) {
 			return list;
 		} catch (JavaModelException e) {
@@ -136,14 +225,26 @@ System.out.println(key.getClassname() + "#" + key.getMethodname());
 		parser.setStatementsRecovery(true);
 		parser.setSource(cu);
 		CompilationUnit unit = (CompilationUnit) parser.createAST(null);
-		MethodVisitor visitor= new MethodVisitor();
+		MethodVisitor visitor= new MethodVisitor(offset);
 		unit.accept(visitor);
 
-		if (visitor.getStatementList() != null && visitor.getStatementList().size() != 0) {
-			List<Value> proposallist = fetchSource(visitor.getStatementList());
-			Collections.sort(proposallist);
-			for (Value v : proposallist) {
-				String str = v.getCount() + " " + v.getClassname() + "." + v.getMethodname();
+		Map<String, ValuewithRanking> proposalmap = new HashMap<String, ValuewithRanking>();
+		for (Value key : visitor.getBeforeStatementList()) { 
+			makeRanking(key, proposalmap, true);
+		}
+		for (Value key : visitor.getAfterStatementList()) { 
+			makeRanking(key, proposalmap, false);
+		}
+
+		List<ValuewithRanking> proposallist = new ArrayList<ValuewithRanking>();
+		for (ValuewithRanking v: proposalmap.values()) {
+			proposallist.add(v);
+		}
+		Collections.sort(proposallist);
+
+		for (ValuewithRanking v : proposallist) {
+			if (v.getPercentage() != 0) {
+				String str = String.format("%3d: %s.%s", v.getPercentage(), v.getClassname(),v.getMethodname());
 				list.add(new CompletionProposal(str, offset, 0, 0, null, str, null, "<pre>"+str+"</pre>"));
 			}
 		}
