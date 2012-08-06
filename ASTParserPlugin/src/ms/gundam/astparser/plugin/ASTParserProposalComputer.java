cb4079ee8e05bf33b1dbe35d53de514829d0a7c1
Tetsuo Yamamoto <tetsuo@cs.ce.nihon-u.ac.jp>
2012-08-06 19:44:04 +0900

    delete warning

--- ASTParserPlugin/src/ms/gundam/astparser/plugin/ASTParserProposalComputer.java
+++ ASTParserPlugin/src/ms/gundam/astparser/plugin/ASTParserProposalComputer.java
@@ -2,8 +2,6 @@ package ms.gundam.astparser.plugin;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
