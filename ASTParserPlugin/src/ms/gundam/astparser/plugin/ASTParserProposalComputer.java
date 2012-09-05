8eadffde636e8ee0333a522ba4ec81e79ab98d53
Tetsuo Yamamoto <tetsuo@cs.ce.nihon-u.ac.jp>
2012-09-05 23:28:14 +0900

    fix

--- ASTParserPlugin/src/ms/gundam/astparser/plugin/ASTParserProposalComputer.java
+++ ASTParserPlugin/src/ms/gundam/astparser/plugin/ASTParserProposalComputer.java
@@ -3,7 +3,6 @@ package ms.gundam.astparser.plugin;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
